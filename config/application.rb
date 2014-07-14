87fad366b4c4e0dbf03b182ffbfa85b52b3f4f82
Swapna Gupta <swapna.sony@gmail.com>
2014-07-14 18:28:34 -0500

    config update to prevent heroku from trying to connect to db on deployment

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Healthfundit
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Prevent Heroku from trying to connect to database when deploying
+    config.assets.initialize_on_precompile = false
   end
 end
